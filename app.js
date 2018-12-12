ad3c4755f102bb8c98a879cb7085a75a5ef99041
rendrap <rendrapradana@gmail.com>
2018-12-12 17:41:13 +0700

    add app.js

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-var quiz = new Quiz();
+var questions = [
+  new Question("What is the capital of Indonesia", ["Jakarta", "Bali"],"Jakarta"),
+  new Question("What is the capital of England", ["London", "Washington DC"],"London")
+];
 
-var question1 = new Question("What is the capital of Indonesia", "Jakarta", "Bali");
-var question2 = new Question("What is the capital of England", "London", "Washington DC");
+var quiz = new Quiz(questions);
 
-
-quiz.add(question1);
-quiz.add(question2);
-
-var quizElement = document.getElementById("question");
-
-question.renderIn(quizElement);
-
-var guess0Button = document.getElementById("guess0");
-var guess1Button = document.getElementById("guess1");
-
-playButton.onclick = function() {
-  playlist.play();
-  playlist.renderIn(playlistElement);
-};
+QuizUI.displayNext();
