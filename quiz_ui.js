2626db152f0e1596299d0ebf64a27ec1d131a61c
rendrap <rendrapradana@gmail.com>
2018-12-13 11:34:09 +0700

    add display score method

--- quiz_ui.js
+++ quiz_ui.js
@@ -11,5 +11,19 @@ var QuizUI = {
   displayQuestion: function () {
     this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
   },
+  displayChoices: function () {
+    var choices = quiz.getCurrentQuestion().choices;
+
+    for(var i=0; i < choices.length; i++) {
+      this.populateIdWithHTML("choice" + i, choices[i]);
+      this.guessHandler("guess" + i, choices[i]);
+    }
+  },
+  displayScore: function (){
+    var gameOverHTML = "<h1>Game Over</h1>";
+    gameOverHTML += "<h2>Your score is: " + quiz.score + "</h2>";
+    this.populateIdWithHTML("quiz", gameOverHTML);
+  },
+
 
 };
