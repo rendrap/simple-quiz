0eba381ae57e24ba5a4e0b79c96408c3dd881c2a
rendrap <rendrapradana@gmail.com>
2018-12-13 12:32:00 +0700

    add displayProgress method

--- quiz_ui.js
+++ quiz_ui.js
@@ -14,7 +14,7 @@ var QuizUI = {
   displayChoices: function () {
     var choices = quiz.getCurrentQuestion().choices;
 
-    for(var i=0; i < choices.length; i++) {
+    for(var i = 0; i < choices.length; i++) {
       this.populateIdWithHTML("choice" + i, choices[i]);
       this.guessHandler("guess" + i, choices[i]);
     }
@@ -24,6 +24,23 @@ var QuizUI = {
     gameOverHTML += "<h2>Your score is: " + quiz.score + "</h2>";
     this.populateIdWithHTML("quiz", gameOverHTML);
   },
+
+  populateIdWithHTML: function (id, text){
+    var element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+  guessHandler: function(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function(){
+      quiz.guess(guess);
+      QuizUI.displayNext();
+    };
+  },
+
+  displayProgress: function(){
+    var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+    this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
+  }
 
 
 };
